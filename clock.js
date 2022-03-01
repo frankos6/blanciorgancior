e2458bad63dd1267a73826a7b2a8a40519e86f82
frankos6 <82461244+frankos6@users.noreply.github.com>
2022-03-01 15:02:02 +0100

    :trollface: v1

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 today = new Date();
 
-opening = new Date("April 20, 2022");
+opening = new Date("June 24, 2022");
 msPerDay = 24 * 60 * 60 * 1000 ;
 timeLeft = (opening.getTime() - today.getTime());
 
---
+++ troll.js
@@ -0,0 +1,3 @@
+function troll(){
+    document.write("<img src='images/electrode_trollface.png' width='100%' height='100%'>")
+}
