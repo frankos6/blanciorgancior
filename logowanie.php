ce7c07454f15b67f3b431909ddbdf41d27f33dc2
frankos6 <82461244+frankos6@users.noreply.github.com>
2022-05-10 13:57:18 +0200

    Update logowanie.php

--- logowanie.php
+++ logowanie.php
@@ -1,27 +1,27 @@
-<?php 
-    session_start();
-    require 'login.php';
-    //require 'backend/connection.php';
+<?php
+    require 'login.php';                                                                                                    // Połącznie z login.php
+    require 'backend/connection.php';                                                                                       // Połącznie z bazą danych
+
+    session_start();                                                                                                        // Zaczęcie sesji
+    error_reporting(0);                                                                                                     // Wyłączenie pokazywania błędów
+    ini_set('display_errors', 0);                                                                                           //
+
     if(!isset($_POST['Password'])){
-    echo "<script> window.onload = document.getElementById('error-msg').innerHTML = ''; </script>";
-    }
-    $conn = new mysqli('localhost', 'root', '', 'kalendarz');
-
-
-    $username = $_POST['email'];
-    $password = $_POST['Password'];
-
-    $sql = "SELECT username, password FROM users WHERE email = '$username'";
-    $result = $conn->query($sql);
-    
-    $x = $result->fetch_row();
-
-    if($password == $x[1]){
-        $_SESSION['Imie'] = $x[0];
-        header('Location: index.php');
-    }else{
-        echo "<script> document.getElementById('error-msg').innerHTML = 'Błąd - nieprawidłowe hasło!'</script>";
+    echo "<script> window.onload = document.getElementById('error-msg').innerHTML = ''; </script>";                         // Brak wiadomości błędu w wypadku braku takiego
     }
 
+    $username = $_POST['email'];                                                                                            // Pobranie danych użytkownika
+    $password = $_POST['Password'];                                                                                         // 
 
+    $sql = "SELECT username, password FROM users WHERE email = '$username'";                                                // Mechanizm logowania
+    $result = $conn->query($sql);                                                                                           //
+                                                                                                                                
+    $x = $result->fetch_row();                                                                                              //
+                                                                                                                            
+    if($password == $x[1]){                                                                                                 //
+        $_SESSION['Imie'] = $x[0];                                                                                          // Ustawnianie imienia w sesji
+        header('location: index.php');                                                                                      //
+    }else{                                                                                                                  //
+        echo "<script> document.getElementById('error-msg').innerHTML = 'Błąd - nieprawidłowe hasło lub login!'</script>";  // Błąd logowania
+    }                                                                                                                       //
 ?>  
