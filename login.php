ebc992e8b0e621d77ecbb8c0c6996aade361df18
frankos6 <82461244+frankos6@users.noreply.github.com>
2022-05-11 18:13:37 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="Styles/style.css">
-    <link rel="stylesheet" href="Styles/login-bg.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/login-bg.css">
     <link rel="icon" href="Images/logo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <script src="Scripts/script.js"></script>
-    <link rel="icon" type="image/x-icon" href="/images/calendar-icon.png">
+    <script src="scripts/script.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="Images/favicon.ico" />
 </head>
 <body>
 <div class="center-box">
