d528987a5bf7c595e3f95b58466efdea6eeec72b
Yaneck <92322885+BGTYaneck@users.noreply.github.com>
2022-05-12 09:44:25 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,10 +1,10 @@
-today = new Date();
+// today = new Date();
 
-opening = new Date("May 13, 2022");
-msPerDay = 24 * 60 * 60 * 1000 ;
-timeLeft = (opening.getTime() - today.getTime());
+//opening = new Date("May 13, 2022");
+//msPerDay = 24 * 60 * 60 * 1000 ;
+//timeLeft = (opening.getTime() - today.getTime());
 
-e_daysLeft = timeLeft / msPerDay;
-daysLeft = Math.floor(e_daysLeft) + 1;
+//e_daysLeft = timeLeft / msPerDay;
+//daysLeft = Math.floor(e_daysLeft) + 1;
 
-document.getElementById("clock").innerHTML = daysLeft + " DNI";
+//document.getElementById("clock").innerHTML = daysLeft + " DNI";
