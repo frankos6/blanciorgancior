9a3d08e880ed218da8a50e7d1c719b285dc32c6e
frankos6 <frankos6@wp.pl>
2022-05-26 22:03:41 +0200

    worikng??? :pray:

--- kalendarzbgt/index.php
+++ kalendarzbgt/index.php
@@ -38,7 +38,7 @@
         <div class="rightside">
             <h3 class="card-header" id="monthAndYear"></h3>
             <div class="abcd">
-                    <select name="kalendarz" id="kalendarz" class="index-select" class="form-select" >
+                    <select name="kalendarz" id="kalendarz" class="index-select form-select" >
 
             </select>
             <img class="add-calendar-img" src="/kalendarzbgt/Images/plus-icon.png" onclick="addCalendarForm()" alt="Add icon">
@@ -129,7 +129,7 @@
         </div>
     </div>
     <div class="options-menu" id="options-menu">
-        <div class="overlay" class="w3-container w3-center w3-animate-top">
+        <div class="overlay w3-container w3-center w3-animate-top">
             <iframe id="iframe" src="" width="100%" height="100%"></iframe>
             <button class="login-form-button" id="important" onclick="off()" id="temp"><span>Anuluj</span></button>
         </div>
